cd5e61de968dd69fea58ac9758482bce74b7c2b8
Arne Kiesewetter <banane1996@gmx.net>
2014-06-08 13:33:39 +1200

    Add some method calls and plugin stuff.

---
+++ ChallengeAdvisor/ChallengeAdvisorPlugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ManiaNet.DedicatedServer.Controller.Plugins.ChallengeAdvisor
+{
+    [RegisterPlugin(author: "Banane9", name: "Challenge Advisor")]
+    public class ChallengeAdvisorPlugin : ControllerPlugin
+    {
+        public override bool Load(ServerController controller)
+        {
+            return true;
+        }
+
+        public override void Run()
+        {
+        }
+
+        public override bool Unload()
+        {
+            return true;
+        }
+    }
+}
